a6ed98e45a927bb8d4ae12ed1312a3d9e32e2b6c
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-08-13 15:21:30 +0530

    pilot

---
+++ JS/operators.js
@@ -0,0 +1,21 @@
+// var a=23;
+// var b=34;
+// console.log("The value of a is : ",a);
+// console.log("Sum of a and b is: ",a+b);
+// console.log(a," is greater than " ,b ,":",a>b);
+
+
+var a=23;
+var b="Hello";
+var c=true;
+var d=null;
+var e;
+
+console.log("Data type of a is :",typeof(a));
+console.log("Data type of b is :",typeof(b));
+console.log("Data type of c is :",typeof(c));
+console.log("Data type of d is :",typeof(d));
+console.log("Data type of e is :",typeof(e));
+
+console.log(a+e);
+
---
+++ JS/vars.js
@@ -0,0 +1,23 @@
+// Const var type
+// const a=23;
+// console.log(a);
+// a='23';
+// console.log(a);
+
+// let var type
+// let b=23;
+// console.log("Before scope",b);
+// {
+//     let b=34;
+//     console.log("Inside scope",b);
+// }
+// console.log("After scope",b);
+
+// Var var type
+// var b=23;
+// console.log("Before scope",b);
+// {
+//     var b=34;
+//     console.log("Inside scope",b);
+// }
+// console.log("After scope",b);
