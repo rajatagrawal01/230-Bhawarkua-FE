f9c230966e18bca0838ab085e597cf4da8e6c262
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-09-18 16:31:02 +0530

    temp,promise

---
+++ AdvJS/advConditions.js
@@ -0,0 +1,48 @@
+// if(condition){
+//     //operation
+// }
+// var age=23;
+// var result;
+// if(age>=18){
+//     result="Adult"
+// }
+// else{
+//     result = "Minor"
+// }
+
+// console.log(age>=18);
+
+// let result = age>=18 ? "Adult": "Minor"
+
+
+// // Logical operators as condition
+// let isLogged=true
+// if(isLogged){
+//     console.log("Welcome");
+// }
+
+// isLogged && console.log("Welcome");
+
+// // OR
+// let name = inputName || "Guest"
+
+// console.log();
+
+
+// function output(user){
+//     if(user.usrstatus!=="Active") return;
+//     console.log("Welcome");
+//     console.log("Your id is ....");
+// }
+// let userName={
+//     userNm:"Rajat",
+//     usrstatus:"Active"
+// }
+
+// output(userName)
+
+
+
+if(await checkUser()){
+    console.log("Enter");
+}
